8bfeab8acc28793b1225d5a9b99116e02596580f
cameron penner <camdpenner@gmail.com>
2016-08-07 14:47:42 -0500

    users can give themselves certain roles

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -2,6 +2,7 @@
 using Discord.Commands;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace DiscordBot
 {
@@ -36,12 +37,11 @@ namespace DiscordBot
 			try
 			{   // Open the text file using a stream reader.
 				StreamReader sr = new StreamReader("token.txt");
-				
-					// Read the stream to a string, and write the string to the console.
-					token = sr.ReadToEnd();
 
-					Console.WriteLine($"Got token: {token}");
-				
+				// Read the stream to a string, and write the string to the console.
+				token = sr.ReadToEnd();
+
+				Console.WriteLine($"Got token: {token}");
 			}
 			catch(Exception e)
 			{
@@ -50,7 +50,7 @@ namespace DiscordBot
 			}
 
 			CreateCommands();
-			
+
 			_client.ExecuteAndWait(async () =>
 			{
 				await _client.Connect(token);
@@ -75,8 +75,36 @@ namespace DiscordBot
 				.Parameter("edition", ParameterType.Unparsed)
 				.Do(async (e) =>
 				{
-					string message = $"We're still working on this feature...";
-					await e.Channel.SendMessage(message);
+					string[] openRoles = new string[] { "@5e", "@pathfinder", "@3.5" };
+					string edition = e.GetArg("edition");
+
+					bool found = false;
+
+					foreach(var role in openRoles)
+					{
+						if(edition.Equals(role))
+						{
+							found = true;
+						}
+					}
+
+					if(found)
+					{
+						await e.User.AddRoles(e.Server.Roles.Where(x => x.Name == edition).First());
+						string message = $"@{e.User.Name}, you are now following {edition}!";
+						await e.Channel.SendMessage(message);
+					}
+					else
+					{
+						string message = "That role doesn't exist, try one of: ";
+
+						foreach(var role in openRoles)
+						{
+							message += "[" + role + "] ";
+						}
+
+						await e.Channel.SendMessage(message);
+					}
 				});
 		}
 
